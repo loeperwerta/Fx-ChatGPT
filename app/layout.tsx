c2ad55d1513d169ac386f0ff314d0dee62812356
loeperwerta <138773900+loeperwerta@users.noreply.github.com>
2023-07-06 19:32:48 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./styles/highlight.scss";
 import { getClientConfig } from "./config/client";
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "福兴集团 ChatGPT",
   description: "Your personal ChatGPT Chat Bot.",
   viewport: {
     width: "device-width",
